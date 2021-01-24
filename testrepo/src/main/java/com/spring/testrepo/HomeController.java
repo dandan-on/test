137f8320cd31f04bcdff3eaa77fc8d186bc3b09f
dan <leeonlydan@gmail.com>
2021-01-24 11:48:12 +0900

    add println2

--- testrepo/src/main/java/com/spring/testrepo/HomeController.java
+++ testrepo/src/main/java/com/spring/testrepo/HomeController.java
@@ -34,6 +34,8 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		System.out.println("Hello World");
+		System.out.println("Hello World@@@");
+		System.out.println("Hello World!!!");
 		
 		return "home";
 	}
